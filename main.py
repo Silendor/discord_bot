d22ab2f438a14a5251050185315232dc4bcb2d6e
Silendor <hermit256@gmail.com>
2018-09-19 01:59:11 +0400

    add logging to file

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
-import discord
-from discord.ext import commands
 import os
+import logging
+import json
+import aiohttp
+import asyncio
+import discord
+from discord import Game
+# from discord.ext import commands
+from discord.ext.commands import Bot
 
 secret='DISCORD_BOT_SECRET'
+description = '''Simple bot'''
+
+# set up logging
+# log output to console:
+# logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.ERROR)
+# log to file:
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 def initialize_token():
     with open('.env', 'r') as filetype:
         os.environ[secret] = filetype.read().strip()
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='$', description='Newsletter bot')
+# client = discord.Client()
+client = Bot(command_prefix='$', description=description)
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="с людишками"))
+    # await client.change_presence(game=Game(name="с людишками"))
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -26,18 +44,26 @@ async def on_message(message):
         return
     # if message.author != client.user:
     #     await client.send_message(message.channel, message.content[::-1])
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+
+    if message.content.startswith('?help'):
         await client.send_message(message.channel, msg)
+################################################################
+# experimental
+################################################################
+url = 'https://pubg.op.gg/user/Silendor?server=pc-ru'
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":wave: Hello, there!")
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 
-@bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-
+################################################################
+# /experimental
+################################################################
 initialize_token()
 token = os.environ.get(secret)
 client.run(token)
