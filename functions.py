5cf4d8cba3994235510074830357ff0c4ee02d19
Silendor <hermit256@gmail.com>
2018-09-29 18:11:08 +0400

    add cache

--- functions.py
+++ functions.py
@@ -2,6 +2,17 @@ import os
 import json
 import requests
 import asyncio
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+# caching
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': '/tmp/cache/data',
+    'cache.lock_dir': '/tmp/cache/lock'
+}
+ 
+cache = CacheManager(**parse_cache_config_options(cache_opts))
 
 def api_call(api_key, url):
     header = {
@@ -14,6 +25,7 @@ def api_call(api_key, url):
     return r.json()
     # return requests.get(url, headers=header)
 
+@cache.cache('get_player_func', type="file", expire=300)
 def get_player_info(api_key, shard, player_name):
     '''get info about player'''
     url_player = "https://api.pubg.com/shards/{}/players?filter[playerNames]={}".format(shard, player_name)
@@ -24,18 +36,20 @@ def get_player_info(api_key, shard, player_name):
         player_id = player_info['errors'][0]['title']
     return player_id
 
+@cache.cache('get_season_func', type="file", expire=2592000)
 def get_season_info(api_key, shard):
     '''get info about current season'''
     url_season = "https://api.pubg.com/shards/{}/seasons".format(shard)
-    season_id = 'division.bro.official.2018-09'
-    # season_info = api_call(api_key, url_season)
-    # season_num = len(season_info['data'])
-    # if season_info['data'][season_num-1]['attributes']['isCurrentSeason']:
-    #     season_id = season_info['data'][season_num-1]['id']
-    # else:
-    #     season_id = 'Not Found'
+    # season_id = 'division.bro.official.2018-09'
+    season_info = api_call(api_key, url_season)
+    season_num = len(season_info['data'])
+    if season_info['data'][season_num-1]['attributes']['isCurrentSeason']:
+        season_id = season_info['data'][season_num-1]['id']
+    else:
+        season_id = 'Not Found'
     return season_id
 
+@cache.cache('get_season_func', type="file", expire=300)
 def get_general_stat_info(api_key, shard, player_id, season_id,
                             interest_mode, interest_items):
     '''get info about user statistics'''
